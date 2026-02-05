fd6af80d05a55bd8b6cca1638d8662a67c85b874
toms38 <tariqaziz32492@gmail.com>
2026-02-05 11:29:03 +0000

    datatypes

---
+++ 1st.js/2nd.js
@@ -0,0 +1,19 @@
+console.log("Types of datatypes, and something more");
+//"use strict "  treat all vs code as newer version
+// alert() ,, we are using node.js not browseer
+//readibility is important 
+
+//datatypes
+// numbers = 2 to pwr 53
+// string
+// bigint
+// boolean 
+// null=standalone value ...empty 
+// undefined = not defined nut declared 
+//symbol= uniqueness 
+// object
+
+console.log(typeof 3);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
