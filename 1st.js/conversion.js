bd1d7a30c2ecb0969f9034ad9132aa96efb7ec2f
toms38 <tariqaziz32492@gmail.com>
2026-02-05 16:34:46 +0000

    conversion of datatypes

---
+++ 1st.js/conversion.js
@@ -0,0 +1,72 @@
+//nmber conversion
+// let score=33;
+//console.log(typeof score); //number
+//console.log(typeof (score)); //number
+
+
+// let score="33";
+// console.log(typeof score);
+// let value=Number(score);
+// console.log(typeof value);   //string convergt into number
+
+// let score="33abc";
+// console.log(typeof score);
+// let value=Number(score);
+// console.log(typeof value);
+// console.log(value); //NaN
+
+
+// let score=null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let value=Number(score);
+// console.log(typeof value);
+// console.log( value);
+
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let value=Number(score);
+// console.log(typeof value);
+// console.log( value);
+
+
+// let score=true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let value=Number(score);
+// console.log(typeof value);
+// console.log( value);
+
+
+// let score="Hitesh"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let value=Number(score);
+// console.log(typeof value);
+// console.log( value);
+
+
+// let isLogin=1;
+// let valueofisLogin=Boolean(isLogin);
+// console.log(valueofisLogin);
+
+
+
+// let isLogin=""
+// let valueofisLogin=Boolean(isLogin);
+// console.log(valueofisLogin);//false
+
+
+// let isLogin="   "
+// let valueofisLogin=Boolean(isLogin);
+// console.log(valueofisLogin);
+
+let number=38;
+let stringNumber=String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
