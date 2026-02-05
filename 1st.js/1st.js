97d920f1258ce1adceb61a5ce31b30173495b57f
toms38 <tariqaziz32492@gmail.com>
2026-02-05 11:08:14 +0000

    let , var and const

---
+++ 1st.js/1st.js
@@ -0,0 +1,18 @@
+//console.log("const, let and var ki kahani ");
+/*
+const change nai hosaktha , let ar var chanage ho skthy hyn update ho skthy hyn
+let scope ko keep rktha hynn jbky var nai 
+*/
+// const accountId=234;
+// let email="aziz@gmail.com"
+// var password="12345"
+// city="peshawar"
+// let district; //thsi is undefined
+// console.table([accountId, email, password,city,district])
+
+// console.log(accountId);
+
+
+
+
+
