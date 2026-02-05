6aeb933dd91772e308fed0f97065fda30d1f53b0
toms38 <tariqaziz32492@gmail.com>
2026-02-05 17:10:33 +0000

    operations

---
+++ 1st.js/operations.js
@@ -0,0 +1,41 @@
+// let value=38;
+// let negValue=-value;
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+//concatenation
+// let str1="TARIK"
+// let str2="AZIZ"
+// let str3=str1+str2;
+// console.log(str3);
+
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+"2");
+// console.log("1"+2+2); //string first so string atm hhh
+// console.log(1+2+"2"); //number first so number atm hhh
+
+
+// console.log(+true); //1 become a positibe number
+// console.log(+""); //0
+
+// let countNumber=100
+// countNumber++;
+// console.log(countNumber);
+
+// ++countNumber;
+// console.log(countNumber);
+
+
+
+
+
+
