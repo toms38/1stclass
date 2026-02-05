49f8a659e0502c82f56e279f7cb24471283ea392
toms38 <tariqaziz32492@gmail.com>
2026-02-05 18:22:28 +0000

    datatypesummary

---
+++ 1st.js/datatypessummary.js
@@ -0,0 +1,29 @@
+//JavaScript is dynamically typed language
+// Two types 
+// Primitive 07 (by value)
+//-> string number boolean null undefined bigInt symbol
+
+// Non Primitive (by reference)
+//array objects functions
+
+// let id =Symbol('123')
+// let anotherId =Symbol('123')
+// console.log(id===anotherId);
+
+const bigNumber=8839299392283928n
+console.log(typeof bigNumber);
+
+// let array=["ali", "tarik", "aziz"];
+// console.log(typeof array); //object
+
+// const myFunction =Function(){
+//     console.log("hello toms");
+    
+// }
+
+let myObj={
+    surname:"toms",
+    age:20,
+};
+
+
