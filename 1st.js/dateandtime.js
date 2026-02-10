56aa593aa1fba0cebfc78a7c3d7c72fb90ffd240
toms38 <tariqaziz32492@gmail.com>
2026-02-10 17:40:55 +0000

    ..

---
+++ 1st.js/dateandtime.js
@@ -0,0 +1,13 @@
+//date ...date is an object in js..
+let myDate= new Date()
+// console.log(myDate); //unreadable date format
+// console.log(myDate.toString()); //Tue Feb 10 2026 17:32:34 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); //Tue Feb 10 2026
+// console.log(myDate.toISOString()); //2026-02-10T17:32:34.214Z
+// console.log(myDate.toJSON()); //2026-02-10T17:32:34.214Z
+// console.log(myDate.toLocaleString()); //2/10/2026, 5:32:34 PM
+// console.log(myDate.toLocaleTimeString()); //5:32:34 PM
+// console.log(typeof myDate);//date is an object interview
+
+let myCreatedDate=new date(12, 0, 2026);
+console.log(myCreatedDate.toString());
