5a41808d62057a55db7486d24c86efba0d71f4e3
toms38 <tariqaziz32492@gmail.com>
2026-03-06 15:29:03 +0000

    array1basic

---
+++ 1st.js/array.js
@@ -0,0 +1,34 @@
+console.log("ARRAY with Methods");
+const array=[1, 2, 3, 4 , 5 ];
+console.log(array);
+
+
+// array.push(8);
+// array.push(3);
+// array.pop();
+
+// array.unshift(888883333);
+// array.shift()
+// console.log(array.includes(9));
+// console.log(array.indexOf(9));
+// console.log(array.indexOf(1));
+
+// const newArray=array.join(); //join convert data into string
+// console.log(newArray);
+// console.log(typeof(newArray));
+
+//SLICE and SPLICE
+const arr1=array.slice(1,3);
+console.log(arr1);
+console.log(array);
+
+
+const arr2=array.splice(1,3);
+console.log(arr2);
+console.log(array);
+
+
+
+
+
+
